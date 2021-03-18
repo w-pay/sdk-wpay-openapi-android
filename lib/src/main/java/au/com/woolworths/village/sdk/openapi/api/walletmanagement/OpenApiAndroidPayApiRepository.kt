fa194fdde618cb008bc14077b6cd1fd0683faacb
shreyas-redcrew <shreyas@redcrew.com.au>
2021-03-18 23:46:59 +1100

    OpenApiAndroidPayApiRepository: tokenize and update method

--- lib/src/main/java/au/com/woolworths/village/sdk/openapi/api/walletmanagement/OpenApiAndroidPayApiRepository.kt
+++ lib/src/main/java/au/com/woolworths/village/sdk/openapi/api/walletmanagement/OpenApiAndroidPayApiRepository.kt
@@ -1,16 +1,70 @@
 package au.com.woolworths.village.sdk.openapi.api.walletmanagement
 
-import au.com.woolworths.village.sdk.ApiResult
+import au.com.woolworths.village.sdk.*
 import au.com.woolworths.village.sdk.model.walletmanagement.TokenizeAndroidPayRequest
 import au.com.woolworths.village.sdk.model.walletmanagement.TokenizeAndroidPayResponse
+import au.com.woolworths.village.sdk.openapi.OpenApiClientFactory
+import au.com.woolworths.village.sdk.openapi.dto.TokenizeAndroidPayRequest
 
-interface OpenApiAndroidPayApiRepository {
-    fun tokenize(
-        tokenizeAndroidPayRequest: TokenizeAndroidPayRequest
-    ): ApiResult<TokenizeAndroidPayResponse>
+class OpenApiAndroidPayApiRepository  (
+    requestHeadersFactory: RequestHeadersFactory,
+    options: VillageOptions
+) : OpenApiClientFactory(requestHeadersFactory, options), AndroidPayApiRepository {
+    override fun tokenize(
+        tokenizeAndroidPayRequest: au.com.woolworths.village.sdk.model.walletmanagement.TokenizeAndroidPayRequest
+    ): ApiResult<TokenizeAndroidPayResponse> {
+        return makeCall {
+            val api = createWalletManagementApi()
 
-    fun update(
+            val body = TokenizeAndroidPayRequest()
+            body.encryptedMessage = tokenizeAndroidPayRequest.encryptedMessage
+            body.ephemeralPublicKey = tokenizeAndroidPayRequest.ephemeralPublicKey
+            body.tag = tokenizeAndroidPayRequest.tag
+            body.publicKeyHash = tokenizeAndroidPayRequest.publicKeyHash
+            body.instrumentType = tokenizeAndroidPayRequest.instrumentType
+            body.primary = tokenizeAndroidPayRequest.primary
+            body.comment = tokenizeAndroidPayRequest.comment
+
+            api.androidpayTokenizePost(
+                getDefaultHeader(api.apiClient, X_API_KEY),
+                "",
+                "",
+                body,
+                "",
+                "",
+                "")
+
+            return@makeCall ApiResult.Success(object : TokenizeAndroidPayResponse {})
+        }
+    }
+
+    override fun update(
         paymentInstrumentId: String,
-        tokenizeAndroidPayRequest: TokenizeAndroidPayRequest
-    ): ApiResult<TokenizeAndroidPayResponse>
+        tokenizeAndroidPayRequest: au.com.woolworths.village.sdk.model.walletmanagement.TokenizeAndroidPayRequest
+    ): ApiResult<TokenizeAndroidPayResponse> {
+        return makeCall {
+            val api = createWalletManagementApi()
+
+            val body = au.com.woolworths.village.sdk.openapi.dto.TokenizeAndroidPayRequest()
+            body.encryptedMessage = tokenizeAndroidPayRequest.encryptedMessage
+            body.ephemeralPublicKey = tokenizeAndroidPayRequest.ephemeralPublicKey
+            body.tag = tokenizeAndroidPayRequest.tag
+            body.publicKeyHash = tokenizeAndroidPayRequest.publicKeyHash
+            body.instrumentType = tokenizeAndroidPayRequest.instrumentType
+            body.primary = tokenizeAndroidPayRequest.primary
+            body.comment = tokenizeAndroidPayRequest.comment
+
+            api.androidpayTokenizePaymentInstrumentIdPost(
+                paymentInstrumentId,
+                getDefaultHeader(api.apiClient, X_API_KEY),
+                "",
+                "",
+                body,
+                "",
+                "",
+                "")
+
+            return@makeCall ApiResult.Success(object : TokenizeAndroidPayResponse {})
+        }
+    }
 }
