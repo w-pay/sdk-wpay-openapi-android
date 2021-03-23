af2a77d1d6246c37e6550f9126b44179311d6aa8
shreyas-redcrew <shreyas@redcrew.com.au>
2021-03-23 13:48:14 +1100

    refactoring the code

--- lib/src/main/java/au/com/woolworths/village/sdk/openapi/model/OpenApiGuestTokenizePaypalResponse.kt
+++ lib/src/main/java/au/com/woolworths/village/sdk/openapi/model/OpenApiGuestTokenizePaypalResponse.kt
@@ -9,24 +9,3 @@ class OpenApiGuestTokenizePaypalResponse(
     override val payPal: au.com.woolworths.village.sdk.model.PaypalDetails
         get() = OpenApiGuestPaypalDetails(tokenizeGooglePayResponse.payPal)
 }
-
-class OpenApiGuestPaypalDetails(
-    private val guestTokenizePaypalResponsePayPal: GuestTokenizePaypalResponsePayPal
-) : au.com.woolworths.village.sdk.model.PaypalDetails{
-    override val paymentInstrumentId: String
-        get() = guestTokenizePaypalResponsePayPal.paymentInstrumentId
-    override val status: au.com.woolworths.village.sdk.model.PaypalStatusEnum
-        get() = au.com.woolworths.village.sdk.model.PaypalStatusEnum.valueOf(guestTokenizePaypalResponsePayPal.status.value)
-    override val allowed: Boolean
-        get() = guestTokenizePaypalResponsePayPal.allowed
-    override val lastUpdated: String
-        get() = Date().toString()
-    override val lastUsed: String
-        get() = Date().toString()
-    override val primary: Boolean
-        get() = false
-    override val payPalId: String
-        get() = ""
-    override val customerId: String
-        get() = ""
-}
--- lib/src/main/java/au/com/woolworths/village/sdk/openapi/model/OpenApiImportPaymentInstrumentsResponse.kt
+++ lib/src/main/java/au/com/woolworths/village/sdk/openapi/model/OpenApiImportPaymentInstrumentsResponse.kt
@@ -13,33 +13,7 @@ class OpenApiImportPaymentInstrumentsResponse(
     override val shopperId: String
         get() = importPaymentInstrumentsResponse.shopperId
     override val creditCards: List<au.com.woolworths.village.sdk.model.walletmanagement.CreditCardResult>
-        get() = importPaymentInstrumentsResponse.creditCards!!.map(::toCreditCards)
-
+        get() = importPaymentInstrumentsResponse.creditCards!!.map { OpenApiCreditCardResult(it) }
     override val payPal: au.com.woolworths.village.sdk.model.walletmanagement.PayPal
-        get() = toPayPal(importPaymentInstrumentsResponse.payPal)
-
-    private fun toCreditCards(importPaymentInstrumentsResponseCreditCards: ImportPaymentInstrumentsResponseCreditCards?):
-            au.com.woolworths.village.sdk.model.walletmanagement.CreditCardResult {
-        return OpenApiCreditCardResult(importPaymentInstrumentsResponseCreditCards!!)
-    }
-
-    private fun toPayPal(importPaymentInstrumentsResponseCreditCards: ImportPaymentInstrumentsResponsePayPal?):
-            au.com.woolworths.village.sdk.model.walletmanagement.PayPal {
-        return OpenApiPayPal(importPaymentInstrumentsResponseCreditCards!!)
-    }
-}
-
-class OpenApiPayPal(
-    private val importPaymentInstrumentsResponseCreditCards: ImportPaymentInstrumentsResponsePayPal
-) : au.com.woolworths.village.sdk.model.walletmanagement.PayPal{
-    override val customerId: String
-        get() = importPaymentInstrumentsResponseCreditCards.customerId
-    override val payPalId: String
-        get() = importPaymentInstrumentsResponseCreditCards.payPalId!!
-    override val paymentMethodToken: String
-        get() = importPaymentInstrumentsResponseCreditCards.paymentMethodToken!!
-    override val result: au.com.woolworths.village.sdk.model.walletmanagement.ResultEnum
-        get() = au.com.woolworths.village.sdk.model.walletmanagement.ResultEnum.valueOf(importPaymentInstrumentsResponseCreditCards.result.value)
-    override val errorMessage: au.com.woolworths.village.sdk.model.walletmanagement.ErrorMessage
-        get() = OpenApiErrorMessage(importPaymentInstrumentsResponseCreditCards.errorMessage.toString())
+        get() = OpenApiPayPal(importPaymentInstrumentsResponse.payPal!!)
 }
