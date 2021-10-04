c56645bc51daed5249b3a1e8cc0b87ccde782aa7
Kieran Simpson <kieran@redcrew.com.au>
2021-10-05 10:08:41 +1100

    Add build pipeline.

---
+++ scripts/tag.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+version=$(./gradlew :sdk:properties | grep '^version:' | sed -e 's/^version: \(.*\)$/v\1/')
+
+# When GH Actions checks out the repo it doesn't pull tags
+echo "Fetching tags"
+git fetch --tags
+
+echo "Checking for ${version}"
+
+if git show-ref --tags $version --quiet; then
+  echo "Tag exists"
+else
+  echo "Tagging with ${version}"
+  git tag $version
+
+  # Need to push to GH to then create a release
+  git push --tags
+
+  echo $GH_AUTH_TOKEN | gh auth login --with-token
+  gh release create $version
+fi
